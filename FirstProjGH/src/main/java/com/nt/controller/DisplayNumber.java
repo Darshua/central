97e1beefe04705fcf776e3bc1739ea853ce78890
Anisha Parida <Anisha Parida@192.168.0.3>
2018-06-12 13:30:24 +0530

    2. commit

--- FirstProjGH/src/main/java/com/nt/controller/DisplayNumber.java
+++ FirstProjGH/src/main/java/com/nt/controller/DisplayNumber.java
@@ -1,5 +1,12 @@
 package com.nt.controller;
 
 public class DisplayNumber {
+	public static void main(String[] args) {
+		System.out.println("Main Method starts");
+		int n = 5;
+		for(int i=0;i<=n;i++) {
+			System.out.println("value =:: "+i);
+		}
+	}
 
 }
